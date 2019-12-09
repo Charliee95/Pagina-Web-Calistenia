7e3474e17419ebdb31e14d12c84a219ed3ca8cd3
Emma <52583526+Victor-emanuel@users.noreply.github.com>
2019-12-09 11:09:34 -0300

    servidor listo al fin cdsm

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var usuarios = [
+    { nombre: 'emanuel', pass: 'barrionuevo' }
+]
+
 var express = require('express');
 
 var path = require('path');
@@ -5,8 +9,6 @@ var path = require('path');
 var bodyParser = require('express');
 
 var servidor = express();
-
-var app = express();
 
 var ruta = path.join(__dirname, './publico')
 
@@ -16,9 +18,20 @@ servidor.use(bodyParser.urlencoded({ extended: false }))
 
 servidor.use(bodyParser.json())
 
-app.post('/api/mensajes', function(consulta, respuesta) {
-    console.log('consulta.body.nombre')
-    respuesta.end('bienvenido al club')
+servidor.post('/api/login', function(consulta, respuesta) {
+    var nombre = consulta.body.nombre
+    var pass = consulta.body.pass
+    var usuario = usuarios.find(function(usuario) {
+        return usuario.nombre === nombre && usuario.pass === pass
+    })
+
+    if (usuario) {
+        respuesta.end('autorizado')
+
+    } else {
+        respuesta.end('no autorizado')
+    }
+
 });
 
 
